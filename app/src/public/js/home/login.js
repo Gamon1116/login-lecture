52634fa2c495fbf44aef54b8ee5f2c2d0cba2320
gamon <blazetime4@gmail.com>
2021-04-14 10:12:25 +0900

    로그인요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,32 @@
 "use strict!"
 
-console.log("hello!");
-console.log("bye");
+const { post } = require("../../../../app");
+
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", function() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    }
+
+    fetch("/login", {
+        //json으로감싸서 stringify으로 문자열로 바꿈 ""안으로 들어감
+        method: "POST",
+        headers: {
+            //내가보내는 타입 명시
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+});
+
+// function login() {
+//     const req = {
+//         id: id.value,
+//         password: password.value,
+//     };
+//     console.log(req);
+// }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,5 +8,6 @@ const ctrl = require("./home.ctrl")
 router.get("/", ctrl.home);
 router.get("/login", ctrl.login);
 
+
 //외부로 내보내는 명령어
 module.exports = router;
