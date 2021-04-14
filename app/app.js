460ccf9e4c7b5b04457050a99581132ab0282c60
gamon <blazetime4@gmail.com>
2021-04-14 09:37:47 +0900

    프런트

--- app/app.js
+++ app/app.js
@@ -10,6 +10,8 @@ const home = require("./src/routes/home");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+//현재위치에서 src폴더안에public폴더를 정적경로로 반환해주겟다
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);      //use -> 미들웨어를 등록해주는 메소드
 
